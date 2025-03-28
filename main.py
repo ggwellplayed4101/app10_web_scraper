6b1694b741a579f59637cf89b6972db753da1e11
Bhargav Chataut <bhargavchataut101@gmail.com>
2025-03-28 14:08:40 -0500

    Add Headers

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import selectorlib
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 
+# To make it seem the http request is from a browser and not a script.
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
 def scrape(url):
     """Scrape the page spurce from the URL"""
-    response = requests.get(url)
+    response = requests.get(url, headers=HEADERS)
     source = response.text
     return source
 
