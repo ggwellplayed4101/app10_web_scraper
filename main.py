108525dcf2a5bba23cda8918884349022412b26a
Bhargav Chataut <bhargavchataut101@gmail.com>
2025-03-28 14:11:21 -0500

    Add Header to requests.get

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
+
 def scrape(url):
     """Scrape the page spurce from the URL"""
     response = requests.get(url, headers=HEADERS)
